313cefdd3ccf8976c1ff43571252a4d348af1a8c
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-06 20:31:43 +0100

    Enhcance generation visualization

--- LabyrinthianDemo/States/MazeState.cs
+++ LabyrinthianDemo/States/MazeState.cs
@@ -22,17 +22,23 @@ public class MazeState
 	/// Current state of the maze.
 	/// </summary>
 	public Maze? Maze { get; private set; }
+	/// <summary>
+	/// Generator that is used(or was used) for maze generation.
+	/// </summary>
+	public MazeGenerator? Generator { get; private set; }
 
 	/// <summary>
-	/// Update a maze state asynchronously.
+	/// Updates the state asynchronously.
 	/// </summary>
 	/// <param name="maze">State of the maze.</param>
+	/// <param name="generator">State of the generator.</param>
 	/// <returns>
 	/// A task that represents an asynchronous operation.
 	/// </returns>
-	public async Task UpdateMazeAsync(Maze? maze)
+	public async Task UpdateMazeAsync(Maze maze, MazeGenerator generator)
 	{
 		Maze = maze;
+		Generator = generator;
 		foreach (var handler in eventHandlers)
 		{
 			await handler.Invoke();
