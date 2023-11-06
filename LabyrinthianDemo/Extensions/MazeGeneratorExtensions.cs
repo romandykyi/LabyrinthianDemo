8dc045c8948e2a706a48d873a763dce0b5a6b02c
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-06 19:11:56 +0100

    Add step-by-step visualization

---
+++ LabyrinthianDemo/Extensions/MazeGeneratorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Labyrinthian;
+
+namespace LabyrinthianDemo.Extensions;
+
+public static class MazeGeneratorExtensions
+{
+	/// <summary>
+	/// Generates a maze step-by-step asynchronously.
+	/// </summary>
+	/// <remarks>
+	/// This method calls <see cref="Task.Run" /> for each maze generation step.
+	/// </remarks>
+	public static async IAsyncEnumerable<Maze> GenerateStepByStepAsync(this MazeGenerator generator)
+	{
+		using IEnumerator<Maze> enumerator = generator.GenerateStepByStep()
+			.GetEnumerator();
+		while (enumerator.MoveNext())
+		{
+			yield return await Task.Run(() => enumerator.Current);
+		}
+	}
+}
