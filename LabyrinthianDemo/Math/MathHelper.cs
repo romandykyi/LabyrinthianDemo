42cbd8d5726778574629e47a8361292eb8f5b8a4
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-07 10:52:31 +0100

    Add advanced settings for algorithms

---
+++ LabyrinthianDemo/Math/MathHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace LabyrinthianDemo.Math;
+
+public static class MathHelper
+{
+	public static float Lerp(float min, float max, float t)
+	{
+		return min * (1f - t) + max * t;
+	}
+	public static double Lerp(double min, double max, double t)
+	{
+		return min * (1.0 - t) + max * t;
+	}
+
+	public static float LerpBias(float min, float max, float t)
+	{
+		return t >= 0.5f ? 
+			Lerp(1f, max, (t - 0.5f) * 2f) : 
+			Lerp(min, 1f, t * 2f);
+	}
+}
