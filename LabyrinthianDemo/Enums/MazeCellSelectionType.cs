8f4d975fc57d3a8c1a3b95b4af1f57b4907270b2
romandykyi <roman.dykyi.8210@gmail.com>
2023-11-07 11:24:18 +0100

    Add cell selection `<MudSelect>`

---
+++ LabyrinthianDemo/Enums/MazeCellSelectionType.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabyrinthianDemo;
+
+public enum MazeCellSelectionType
+{
+	Random,
+	Newest,
+	Latest,
+	[Display(Name = "Newest or random(25%|75%)")]
+	Newest25OrRandom75,
+	[Display(Name = "Newest or random(50%|50%)")]
+	Newest50OrRandom50,
+	[Display(Name = "Newest or random(75%|25%)")]
+	Newest75OrRandom25,
+	[Display(Name = "Latest or random(25%|75%)")]
+	Latest25OrRandom75,
+	[Display(Name = "Latest or random(50%|50%)")]
+	Latest50OrRandom50,
+	[Display(Name = "Latest or random(75%|25%)")]
+	Latest75OrRandom25,
+}
---
+++ LabyrinthianDemo/Math/RandomExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace LabyrinthianDemo.Math;
+
+public static class RandomExtensions
+{
+	/// <summary>
+	/// Returns a random boolean value.
+	/// </summary>
+	/// <param name="random"><see cref="Random" /> class used for random bool generation.</param>
+	/// <param name="weight">Probability of returning <see langword="true" />.</param>
+	/// <returns>
+	/// A random boolean value.
+	/// </returns>
+	public static bool Bool(this Random random, double weight = 0.5f)
+	{
+		return random.NextDouble() <= weight;
+	}
+}
